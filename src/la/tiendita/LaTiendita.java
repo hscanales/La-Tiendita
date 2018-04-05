4ddf8044d350f8af0d7ec7a0a142b159d0b00d7d
Hector Canales <00136317@uca.edu.sv>
2018-04-05 15:09:08 -0600

    INIT

---
+++ src/la/tiendita/LaTiendita.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * 
+ */
+package la.tiendita;
+
+/**
+ *
+ * @author HsCanales <00136317@uca.edu.sv>
+ */
+public class LaTiendita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
